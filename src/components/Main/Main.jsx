6be98b37b524381fc7789e9b1b5f060f7d207da9
Chidozie Ohiaeri <chidozie.ohiaeri@simumatik.com>
2020-12-20 21:15:52 +0100

    Add list component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Card, CardHeader, CardContent, Typography, Grid, Divider} from  '@material-ui/core'
 import makeStyles from './styles'
 import Form from './Form/Form'
+import List from './List/List'
 
 const Main = () => {
     const classes = makeStyles()
@@ -22,7 +23,7 @@ const Main = () => {
             <CardContent className={classes.cartContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        {/* <List /> */}
+                        <List />
                     </Grid>
                 </Grid>
             </CardContent>
