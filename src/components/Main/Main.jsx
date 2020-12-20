db2d44565f2183bc775182c238e659d0657b420f
Chidozie Ohiaeri <chidozie.ohiaeri@simumatik.com>
2020-12-20 20:32:24 +0100

    Main and Details components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Card, CardHeader, CardContent, Typography, Grid, Divider} from  '@material-ui/core'
+import makeStyles from './styles'
+
+const Main = () => {
+    const classes = makeStyles()
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="Powered By Speechly" />
+            <CardContent>
+                <Typography variant="h5" align="center">Total Balance $100</Typography>
+                <Typography variant="subtitle1" style={{lineHeight: "1.5em", marginTop: "20px"}}>
+                    {/* InfoCard */}
+                    Try saying : Add Income for $100 in category salary for Monday...
+                </Typography>
+
+                <Divider />
+                {/* Form */}
+            </CardContent>
+
+            <CardContent className={classes.cartContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        {/* <List /> */}
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
