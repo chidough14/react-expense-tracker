8359eb313848903c5997e98e3a352461e68c2ef3
Chidozie Ohiaeri <chidozie.ohiaeri@simumatik.com>
2020-12-21 10:06:23 +0100

    add speechly

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react'
 import {TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem} from  '@material-ui/core'
 import { v4 as uuidv4} from 'uuid'
+import { useSpeechContext } from '@speechly/react-client'
 import useStyles from './styles'
 import { ExpenseTrackerContext } from '../../../context/context'
 import { incomeCategories, expenseCategories } from '../../../constants/categories'
@@ -17,6 +18,7 @@ const Form = () => {
     const classes = useStyles()
     const [formData, setFormData] = useState(initialState)
     const {addTransaction} = useContext(ExpenseTrackerContext)
+    const { segment } = useSpeechContext()
 
     const createTransaction = () => {
         const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4()}
@@ -31,7 +33,7 @@ const Form = () => {
         <Grid container spacing={2}>
             <Grid item xs={12}>
                 <Typography align="center" variant="subtitle2" gutterBottom>
-                    ...
+                    {segment && segment.words.map(w => w.value).join(" ")}
                 </Typography>
             </Grid>
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {SpeechProvider} from '@speechly/react-client'
 
 import App from './App'
 import './index.css'
 import {Provider} from './context/context'
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
+    <SpeechProvider appId="0aba750c-4037-4fef-b244-1ebed43ea9b6" language="en-US">
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
 document.getElementById('root'))
