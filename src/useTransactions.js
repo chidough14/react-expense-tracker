d35cf06555eeb6439ce1bdece343e64fad6b3109
Chidozie Ohiaeri <chidozie.ohiaeri@simumatik.com>
2020-12-21 08:53:48 +0100

    Chartjs

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -10,10 +10,10 @@ const useTransactions = (title) => {
     const total = transactionsPerType.reduce((acc, currVal) => acc += currVal.amount, 0)
     const categories = title === 'Income' ? incomeCategories : expenseCategories
 
-    console.log({ transactionsPerType, total, categories })
+    console.log({ categories })
 
     transactionsPerType.forEach((t) => {
-        const category = categories.find((c) => c.type === c.category)
+        const category = categories.find((c) => c.type === t.category)
 
         if (category) category.amount += t.amount
     });
@@ -28,7 +28,7 @@ const useTransactions = (title) => {
         labels: filteredCategories.map(c => c.type)
     }
 
-    return { filteredCategories, total, chartData}
+    return {total, chartData}
 }
 
 export default useTransactions
